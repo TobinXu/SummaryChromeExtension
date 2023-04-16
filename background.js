2deadb2db4d3471f55e4dbc306db30d9cb97375c
TobinXu <tommybug@163.com>
2023-04-16 23:39:31 +0800

    update

--- background.js
+++ background.js
@@ -1,41 +1,62 @@
 async function fetchSummary(content) {
-    const apiKey = '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';
+    const apiKey = '';
     const apiEndpoint = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+    const corsProxy = 'https://cors-anywhere.herokuapp.com/';
   
     const prompt = `Please summarize the following content:\n\n${content}\n\nSummary: `;
-    const maxTokens = 1000;
+    const maxTokens = 100;
   
-    const response = await fetch(apiEndpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`
-      },
-      body: JSON.stringify({
-        prompt: prompt,
-        max_tokens: maxTokens,
-        n: 1,
-        stop: null,
-        temperature: 0.7
-      })
-    });
+    try {
+      const response = await fetch(corsProxy + apiEndpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${apiKey}`,
+          'X-Requested-With': 'XMLHttpRequest'
+        },
+        body: JSON.stringify({
+          prompt: prompt,
+          max_tokens: maxTokens,
+          n: 1,
+          stop: null,
+          temperature: 0.7
+        })
+      });
   
-    const jsonResponse = await response.json();
-    return jsonResponse.choices[0].text.trim();
+      if (!response.ok) {
+        throw new Error(`API request failed with status ${response.status}`);
+      }
+  
+      const jsonResponse = await response.json();
+      return jsonResponse.choices[0].text.trim();
+    } catch (error) {
+      console.error('Error fetching summary:', error);
+      return 'An error occurred while fetching the summary. Please try again.';
+    }
   }
+  
   
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === 'summarize') {
       const content = request.content;
-      fetchSummary(content).then((summary) => {
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-          chrome.scripting.executeScript({
-            target: { tabId: tabs[0].id },
-            code: `document.querySelector('body').innerText = \`${summary}\`;`
+      fetchSummary(content)
+        .then((summary) => {
+            console.log('summary', summary.slice(0, 10))
+          chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            chrome.scripting.executeScript({
+              target: { tabId: tabs[0].id },
+              code: `document.querySelector('body').innerText = \`${summary}\`;`
+            });
+          });
+          chrome.runtime.sendMessage({ action: 'showSummary', summary: summary });
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+          chrome.runtime.sendMessage({
+            action: 'showSummary',
+            summary: 'An error occurred while fetching the summary. Please try again.'
           });
         });
-        chrome.runtime.sendMessage({ action: 'showSummary', summary: summary });
-      });
     }
   });
   
--- popup.js
+++ popup.js
@@ -1,8 +1,18 @@
 const summarizeButton = document.getElementById('summarize');
 const summaryContainer = document.getElementById('summary-container');
 const summaryContent = document.getElementById('summary-content');
+const loadingSpinner = document.getElementById('loading-spinner');
+
+function showLoadingSpinner() {
+  loadingSpinner.style.display = 'block';
+}
+
+function hideLoadingSpinner() {
+  loadingSpinner.style.display = 'none';
+}
 
 summarizeButton.addEventListener('click', () => {
+  showLoadingSpinner();
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.scripting.executeScript({
       target: { tabId: tabs[0].id },
@@ -15,5 +25,6 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'showSummary') {
     summaryContent.textContent = request.summary;
     summaryContainer.style.display = 'block';
+    hideLoadingSpinner();
   }
 });
